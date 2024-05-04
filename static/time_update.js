4bb463280fff0a8296e0352ec170fe9486f7e76e
Simisticot <simi.pra@gmail.com>
2024-05-04 22:05:16 +0200

    Keep track of listening progression

--- static/time_update.js
+++ static/time_update.js
@@ -1,8 +1,13 @@
 function update_current_time() {
 	player_control = document.getElementById("player-control");
-	episode_id = player_control.getAttribute("data-episode-id")
-	seconds = Math.trunc(player_control.currentTime)
-	fetch("current-time/" + episode_id + "/" + seconds, { method: "POST" });
+	if( player_control != null && !player_control.paused ){
+		episode_id = player_control.getAttribute("data-episode-id");
+		seconds = Math.trunc(player_control.currentTime);
+		fetch("current-time/" + episode_id + "/" + seconds, { method: "POST" });
+	}
 }
 
-document.getElementById("update-button").addEventListener("click", update_current_time);
+
+window.onload = function() {
+	setInterval(update_current_time, 5000);
+}
